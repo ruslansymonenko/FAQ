b65fe85437bf8b5f6952dfc44905a118d92ce278
Ruslan Symonenko <symonenkors@gmail.com>
2022-12-13 16:51:58 +0200

    finished faq component

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-  
+  const websiteBtns = document.querySelectorAll('.faq-list__item-btn');
+  const faqTexts = document.querySelectorAll('.faq-text');
+
+  function showText (dataAttr) {
+    faqTexts.forEach(item => {
+      let curentListItem = item.closest('.faq-list__item').getAttribute('data-item');
+      if (curentListItem == dataAttr) {
+        item.classList.toggle('faq-text-active');
+      }
+    })
+  }
+
+
+  websiteBtns.forEach(item => {
+    item.addEventListener('click', (e) => {
+      let curentListItem = e.target.closest('.faq-list__item').getAttribute('data-item');
+      showText(curentListItem);
+    })
+  })
 })
